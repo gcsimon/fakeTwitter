72ebf07b1296e85002c91fcdf8b15d882526f109
Giovani Chassot Simon <gsimon@cipucrs.pucrs.br>
2018-08-28 14:09:41 -0300

    initial commit with html

---
+++ src/main/java/hello/PersonForm.java
@@ -0,0 +1,37 @@
+
+package hello;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PersonForm {
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String name;
+
+    @NotNull
+    @Size(min =1, max = 120)
+    private String age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Person(Name: " + this.name + ", Age: " + this.age + ")";
+    }
+}
